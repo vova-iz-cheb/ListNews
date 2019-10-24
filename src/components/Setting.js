7174af68e91ba57056fca95163a3779aa2c6c58e
vova-iz-cheb <vovan1cheb@gmail.com>
2019-10-24 10:40:22 +0300

    Project is ready

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div className="container grow1">
-      <main className="main">
-        <div className="row">
-          <div className="col">
-            <h1>Setting</h1>
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      radio: document.body.style.fontSize || '16px',
+    }
+  }
+
+  handleChange = (e) => {
+    document.body.style.fontSize = e.currentTarget.value;
+    this.setState({radio: e.currentTarget.value});
+  }
+
+  render() {
+    return (
+      <div className="container grow1">
+        <main className="main">
+          <div className="row">
+            <div className="col">
+              <h1>Setting</h1>
+              <form action="" className='setting'>
+                <h2>Размер шрифта:</h2>
+                <label htmlFor="small">Маленький</label>
+                <input onChange={this.handleChange} type="radio" id="small" value="14px" name="fontSize" checked={this.state.radio === '14px'}/><br />
+                <label htmlFor="default">Стандартный</label>
+                <input onChange={this.handleChange} type="radio" id="default" value="16px" name="fontSize" checked={this.state.radio === '16px'}/><br />
+                <label htmlFor="big">Большой</label>
+                <input onChange={this.handleChange} type="radio" id="big" value="18px" name="fontSize" checked={this.state.radio === '18px'}/><br />
+              </form>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
-  )
+        </main>
+      </div>
+    )
+  }
 }
